a6b9a8577a55f86f197208525acdb1a55ca69eee
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-12-24 02:05:30 +0530

    added sign-in and sign-up pages using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <Button>Hello</Button>
-      <Button variant='destructive' className='px-10'>Delete</Button>
+      {/* <h1 className='text-4xl'>Evently</h1> */}
     </main>
   )
 }
