418274b280c5e190b935bb2229f1246269ad0b93
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-12-25 02:09:33 +0530

    added shadcn ui sheet and updated navbar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,13 +1,14 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex flex-center items-center justify-between">
+      <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image
             src="/assets/images/logo.svg"
@@ -16,9 +17,15 @@ const Header = () => {
             alt="Evently Logo"
           />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
